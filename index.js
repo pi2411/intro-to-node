59c1ac36007f08eb29b07627dffbc1c9021e1681
maoz na <you@example.com>
2021-06-21 10:21:10 +0300

    intro-to-node

---
+++ index.js
@@ -0,0 +1,11 @@
+//jshint esversion:6
+var superheroes = require("superheroes");
+var mySuperHeroName = superheroes.random();
+var snakeNames = require("snake-names");
+console.log(mySuperHeroName);
+
+const supervillains = require('supervillains');
+var mySuperVallianName = supervillains.random();
+var mySnakeNames = snakeNames.random();
+console.log(mySuperVallianName);
+console.log(mySnakeNames);
